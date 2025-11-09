48a26ffc216e4007a2553c66e1d09f80d610eac0
Juan Carlos Lloret <juancarloslloret93@gmail.com>
2025-11-09 11:52:53 +0100

    Configured prettier and biome

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,40 @@
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Metadata, Socials } from '@types';
 
 export const SITE: Site = {
-  NAME: "juancarlosllh.dev",
-  EMAIL: "juancarloslloret93@gmail.com",
-  NUM_POSTS_ON_HOMEPAGE: 3,
-  NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+	NAME: 'juancarlosllh.dev',
+	EMAIL: 'juancarloslloret93@gmail.com',
+	NUM_POSTS_ON_HOMEPAGE: 3,
+	NUM_WORKS_ON_HOMEPAGE: 2,
+	NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "juancarlosllh.dev is my personal blog and portfolio.",
+	TITLE: 'Home',
+	DESCRIPTION: 'juancarlosllh.dev is my personal blog and portfolio.',
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+	TITLE: 'Blog',
+	DESCRIPTION: 'A collection of articles on topics I am passionate about.',
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+	TITLE: 'Work',
+	DESCRIPTION: 'Where I have worked and what I have done.',
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+	TITLE: 'Projects',
+	DESCRIPTION: 'A collection of my projects, with links to repositories and demos.',
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "github",
-    HREF: "https://github.com/juancarlosllh"
-  },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/juan-carlos-lloret-hernández-9a7699107",
-  }
+	{
+		NAME: 'github',
+		HREF: 'https://github.com/juancarlosllh',
+	},
+	{
+		NAME: 'linkedin',
+		HREF: 'https://www.linkedin.com/in/juan-carlos-lloret-hernández-9a7699107',
+	},
 ];
